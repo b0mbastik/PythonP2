6f780abcc034cec9b09cc2eacf16cab715f8ee6d
as628 <andrewedwardpetersmith@gmail.com>
2024-03-19 16:26:54 +0000

    Changed name of refined_images to refined_graphs, and added graph generation to the jupyter notebook

--- tests.py
+++ tests.py
@@ -1,19 +1,19 @@
 import unittest, io, sys, time, os
-from data_refinement import RefineData
+import data_refinement as dr 
 
 class TestRefinedData(unittest.TestCase):
     def setUp(self):
-        self.df = RefineData.read_data('refined_census2011.csv')
+        self.df = dr.read_data('refined_census2011.csv')
 
     def test_read_data(self):
-        self.assertTrue(self.df.compare(RefineData.read_data('refined_census2011.csv')).empty)
-        self.assertRaises(FileNotFoundError, RefineData.read_data, 'census2010.csv')
+        self.assertTrue(self.df.compare(dr.read_data('refined_census2011.csv')).empty)
+        self.assertRaises(FileNotFoundError, dr.read_data, 'census2010.csv')
 
     def test_refine_dataset(self):
-        df = RefineData.read_data('census2011.csv')
-        self.assertTrue(self.df.compare(RefineData.refine_dataset(df)).empty)
+        df = dr.read_data('census2011.csv')
+        self.assertTrue(self.df.compare(dr.refine_dataset(df)).empty)
 
     def test_save_refined_data(self):
-        RefineData.save_refined_data(self.df, 'refined_testData.csv')
-        self.assertTrue(self.df.compare(RefineData.read_data('refined_testData.csv')).empty)
+        dr.save_refined_data(self.df, 'refined_testData.csv')
+        self.assertTrue(self.df.compare(dr.read_data('refined_testData.csv')).empty)
         os.remove('refined_testData.csv')
