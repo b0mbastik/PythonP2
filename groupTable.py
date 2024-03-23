a8d83160a93f7a980881e6ccea6e9a5eebe8b1bc
as628 <andrewedwardpetersmith@gmail.com>
2024-03-23 15:04:07 +0000

    Optimised refined_graphs to instead use sort_index() instead of the complicated operation done in get_sorted_columns()

--- groupTable.py
+++ groupTable.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+#Returns a groupby table by taking dataframes containing the census and census variable names, and the variable names to use as rows and columns
+#Uses value_counts on the rows so that they represented the number of records, and unstack to format the given variable for the columns properly
+#Using the data from the groupby table, another dataframe table is created with the same values but column names modified to match the textual represenations of the alphanumeric values
 def getTable(df, vf, rows, columns):
     dt = df.groupby([rows, columns], observed=True)[rows].value_counts().unstack(columns).fillna(0)
     vrows = vf[rows].dropna()
--- refined_graphs.py
+++ refined_graphs.py
@@ -1,30 +1,21 @@
 import pandas as pd
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import json
 
-def get_sorted_arrays(refinedColumn, variableColumn):
-    if (len(refinedColumn.dropna().unique()) != len(variableColumn.dropna())):
-        raise ValueError(f"Provided columns for codes and their corresponding variable names are not the same length.\nColumn 1: {len(refinedColumn.dropna().unique())}\nColumn 2: {len(variableColumn.dropna())}")
-    
-    codes = np.sort(list(map(str, refinedColumn.unique())))
-    columnDict = dict(zip(codes, variableColumn.dropna()))
-    sorted_column = json.loads(refinedColumn.value_counts().to_json())
-    arr = []
+#Returns a tuple containing a column with the value counts of the column containing alphanumeric codes sorted in order of index name and the column containing the textual representation of the codes as a tuple
+#Checks if both columns are the same length, as there should be a textual representation for every alphanumeric code
+def get_sorted_columns(rc, vc):
+    if (len(rc.dropna().unique()) != len(vc.dropna())):
+        raise ValueError(f"Provided columns for codes and their corresponding variable names are not the same length.\nColumn 1: {len(rc.dropna().unique())}\nColumn 2: {len(vc.dropna())}")
+    return (rc.value_counts().sort_index(), vc.dropna())
 
-    for k in sorted_column.keys():
-        arr.append(columnDict[k])
-
-    return (arr,sorted_column)
-
+#Creates a bar chart using the value counts of a given census dataframe variable as the y axis, and the x axis labels as the textual representation of the variable's codes
+#Calls get_sorted_columns() to return a tuple containing the sorted value counts of the variable, and to check that there is a textual representation for every code
+#Graph is saved into the images directory using savefig()
 def get_bar_chart(refinedColumn, variableColumn, x, y, title, name):
+    c = get_sorted_columns(refinedColumn, variableColumn)
     fig, ax = plt.subplots()
-    arrTuple = get_sorted_arrays(refinedColumn, variableColumn)
-    arr = arrTuple[0]
-    sorted_column = arrTuple[1]
-    
-    ax.bar(np.array(arr), np.array([*sorted_column.values()]), color ='maroon', width = 0.4)
+    ax.bar(c[1], c[0], color ='maroon', width = 0.4)
     plt.xlabel(x, fontsize=15, labelpad=15)
     plt.ylabel(y, fontsize=15, labelpad=15)
     fig.set_figwidth(7)
@@ -33,17 +24,18 @@ def get_bar_chart(refinedColumn, variableColumn, x, y, title, name):
     ax.set_title(title, pad=25, fontsize=25)
     plt.savefig(("images/" + name), bbox_inches='tight')
 
+#Creates a pie chart using the value counts of a given census dataframe variable as the sectors of the pie, and the labels as the textual representation of the variable's codes
+#Calls get_sorted_columns() to return a tuple containing the sorted value counts of the variable, and to check that there is a textual representation for every code
+#Graph is saved into the images directory using savefig()
 def get_pie_chart(refinedColumn, variableColumn, title, legend_title, name):
+    c = get_sorted_columns(refinedColumn, variableColumn)
     fig, ax = plt.subplots(figsize=(4,2))
-    arrTuple = get_sorted_arrays(refinedColumn, variableColumn)
-    arr = arrTuple[0]
-    sorted_column = arrTuple[1]
-    
-    ax.pie(np.array([*sorted_column.values()]), labels = arr, radius = 4, autopct='%1.1f%%')
+    ax.pie(c[0], labels = c[1], radius = 4, autopct='%1.1f%%')
     ax.set_title(title, pad=220, fontsize=25)
     plt.legend(title=legend_title, loc='center right', bbox_to_anchor=(6,1), fontsize='xx-large')
     plt.savefig(("images/" + name), bbox_inches='tight')
 
+#Creats a set of bar charts from a groupby table by iterating through every column and making a bar chart for each one
 def get_table_bar_chart(dt, xlabel, ylabel, width, height):
     fig, axs = plt.subplots(len(dt.columns))
     i = 0
@@ -60,6 +52,7 @@ def get_table_bar_chart(dt, xlabel, ylabel, width, height):
         i += 1
     return axs
 
+#Creats a 3D bar chart from a groupby table by iterating through each column in the table, adding the values to the 3D bar chart and then using axis functions to assign relevant values and settings to the axis labels
 def get_table_3d_chart(dt, xlabel, ylabel, zlabel, width, height):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
@@ -76,6 +69,7 @@ def get_table_3d_chart(dt, xlabel, ylabel, zlabel, width, height):
     ax.set_ylabel(ylabel, labelpad=110, fontsize=25)
     ax.set_zlabel(zlabel, labelpad=40, fontsize=25)
 
+#Automatically generates the bar and pie charts detailed in the basic specification
 if __name__ == '__main__':
 
     df = pd.read_csv('refined_census2011.csv')
