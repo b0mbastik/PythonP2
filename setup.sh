9d88025532663615adbc61620ba21a3683390242
Arnav-Bista <arnav.bista12@gmail.com>
2024-03-28 15:54:08 +0000

    Detect if venv already exists before making a new one.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-echo Creating the venv...
-python3 -m venv venv
-echo Created!
+
+if [ -d venv ]
+then
+  echo Virtual Environemnt under 'venv' already exists!
+else 
+  echo Creating the Virtual Environemnt under 'venv'...
+  python3 -m venv venv
+  echo Created!
+fi
+
 source venv/bin/activate
-echo Installing Dependencies
+echo Installing Dependencies...
 pip install -r requirements.txt
 echo Done!
